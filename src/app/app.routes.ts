6d952ee7b7f0eef018f337a140624e758e50e10f
Matthew-The-Developer <Hey.Matthew.Watson@icloud.com>
2024-06-10 10:14:12 -0400

    - initialized project - added angular material - added color palettes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ButtonsComponent } from './buttons/buttons.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'buttons', component: ButtonsComponent },
+  { path: '**', redirectTo: '/buttons' }
+];
